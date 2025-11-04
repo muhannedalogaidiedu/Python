5e136fb64fd2ba288ed9435ad0fc38ec777c0bd0
muhannedalogaidiedu <muhannedalogaidie.edu@gmail.com>
2025-11-04 00:31:02 -0500

    Add all Files and Folder

---
+++ Python-Challenges/HelloEmilie.py
@@ -0,0 +1,10 @@
+# HelloEmilie.py
+
+# Emilie Helen Wolf
+# July 29, 2021
+
+# You have to install the official Python installer to be able to double-click the .py file.
+# I tried using Anaconda, but it did not work.
+
+print("Hello, Emilie!")
+input("\nPress any key to exit.")
---
+++ Python-Challenges/PrintFavoriteQuote.py
@@ -0,0 +1,14 @@
+# PrintFavoriteQuote.py
+
+# Emilie Helen Wolf
+# July 29, 2021
+
+print("""
+      My Favorite Quote
+      
+"If it makes you happy, it doesn't have to make sense to anyone else."
+                        
+                        ~ Unknown
+""")
+
+input("Press any key to exit.")
